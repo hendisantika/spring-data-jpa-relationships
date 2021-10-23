e797d9dd7da6d3f489445632fabeab2cf4828626
Hendi Santika <hendisantika@gmail.com>
2021-10-23 08:03:52 +0700

    #7 - Add Author Entity

---
+++ src/main/java/com/hendisantika/entity/Author.java
@@ -0,0 +1,46 @@
+package com.hendisantika.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Collection;
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-data-jpa-relationships
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/10/21
+ * Time: 08.03
+ */
+@Data
+@Entity
+@Table(name = "author")
+@NoArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "author_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    private Date birthDate;
+
+    @ManyToMany(mappedBy = "authors")
+    private Collection<Book> books;
+}
